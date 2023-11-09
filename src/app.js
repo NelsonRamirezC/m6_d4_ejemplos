4e1dffbdaebd9465bab665e203653ecd3ae46fa4
Nelson Ramirez <xhyskoax@gmail.com>
2023-11-09 10:40:36 -0300

    Se crea filtro de productos por id, recibiendo parámetros por query string

--- src/app.js
+++ src/app.js
@@ -13,11 +13,26 @@ const leerData = async () => {
     return JSON.parse(datos);
 }
 
-//endpoints
+//endpoints todos los productos
 app.get("/api/productos", async (req, res) => {
     let data = await leerData();
     
     res.json({data: data.productos, cantidad: data.productos.length });
+})
+
+//endpoint devolver productos por id
+app.get("/api/productos/filter/id", async (req, res) => {
+    let data = await leerData();
+    let productos = data.productos;
+    let {id} = req.query;
+    
+    let productoBuscado = productos.find(producto => producto.id == id);
+
+    if(productoBuscado){
+        res.json({producto: productoBuscado})
+    }else{
+        res.status(404).json({code: 404, message: "Producto no encontrado."});
+    }
 })
 
 
