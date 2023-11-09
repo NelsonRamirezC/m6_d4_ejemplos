0f49df62ec6ff23779901f7d4e21eb60bbfcd256
Nelson Ramirez <xhyskoax@gmail.com>
2023-11-09 12:28:41 -0300

    se crean 3 filtros de nombre (busqueda exacta, que comienza con... y por patrón)

--- src/app.js
+++ src/app.js
@@ -22,16 +22,18 @@ const leerData = async () => {
 //endpoints todos los productos
 app.get("/api/productos", async (req, res) => {
     let data = await leerData();
-    
-    res.json({data: data.productos, cantidad: data.productos.length });
+    res.json({productos: data.productos, cantidad: data.productos.length });
 })
 
 //endpoint devolver productos por id
 app.get("/api/productos/filter/id", async (req, res) => {
     let data = await leerData();
     let productos = data.productos;
+    //obtenemos de la petición del cliente el parámetro id
     let {id} = req.query;
     
+    //utilizamos el método find de los arrays para buscar el producto
+    //si lo encuentra devuelve el elemento, de lo contrario retorna un undefined
     let productoBuscado = productos.find(producto => producto.id == Number(id));
 
     if(productoBuscado){
@@ -40,6 +42,38 @@ app.get("/api/productos/filter/id", async (req, res) => {
         res.status(404).json({code: 404, message: "Producto no encontrado."});
     }
 })
+
+
+//endpoint filtrado por nombre
+app.get("/api/productos/filter/nombre", async (req, res) => {
+    let data = await leerData();
+    let productos = data.productos;
+    let {nombre} = req.query;
+
+    if(nombre){
+        //filtro busqueda por nombre exacto
+        //let filtroProductos = productos.filter(producto => producto.nombre.toLowerCase() == nombre.toLowerCase());
+        
+        //filtro busqueda por nombre que inicie con...
+        /* let filtroProductos = productos.filter(producto => producto.nombre.toLowerCase().startsWith(nombre.toLowerCase())); */
+
+        //filtro por patrón usando regex
+        let regexNombre = new RegExp(nombre, "i");
+
+        /* console.log(typeof  regexNombre);
+        console.log(regexNombre); */
+
+        let filtroProductos = productos.filter(producto => regexNombre.test(producto.nombre));
+    
+        res.json({productos: filtroProductos, cantidad: filtroProductos.length });
+    }else {
+        res.json({productos, cantidad: productos.length });
+    }
+
+
+})
+
+
 
 
 export default app;
