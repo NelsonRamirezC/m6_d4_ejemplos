ee98820ff72d697857666facd8fb93ab7b6e16d2
Nelson Ramirez <xhyskoax@gmail.com>
2023-11-09 10:25:34 -0300

    Se crea endpoint /api/productos que devuelve el listado total de productos

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
 import express from "express";
+import fs from "fs";
+
+import * as path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+
+const leerData = async () => {
+    let rutaArchivo = path.join(__dirname, "/data/productos.json");
+    let datos = fs.readFileSync(rutaArchivo, "utf8");
+    return JSON.parse(datos);
+}
+
+//endpoints
+app.get("/api/productos", async (req, res) => {
+    let data = await leerData();
+    
+    res.json({data: data.productos, cantidad: data.productos.length });
+})
+
 
 export default app;
